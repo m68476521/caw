91e4ac9d2294badc3c22f092f77961365f605e0d
Miguel Orozco Delgado <miguel.orozco.delgado@gmail.com>
2025-03-19 10:14:11 -0700

    Use arrow to handle API errors
    Develop by Miguel Orozco Delgado <miguel.orozco.delgado@gmail.com>

--- app/src/main/java/com/morozco/mycaw/network/ApiRepository.kt
+++ app/src/main/java/com/morozco/mycaw/network/ApiRepository.kt
@@ -1,7 +1,10 @@
 package com.morozco.mycaw.network
 
+import arrow.core.Either
 import javax.inject.Inject
 
 open class ApiRepository @Inject constructor(private val apiService: ApiService) {
-    open suspend fun getCountries() = apiService.getCountries()
+    open suspend fun getCountries(): Either<Throwable, List<ItemResponse>> = Either.catch {
+        apiService.getCountries()
+    }
 }
