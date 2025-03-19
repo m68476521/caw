57f98e5157bb34162e34a5673fe5bdb7723cf004
Miguel Orozco Delgado <miguel.orozco.delgado@gmail.com>
2025-03-18 22:38:11 -0700

    Add depencies and Home Screen
    In this commit, I add some dependencies:
    
    -Serializable to use to nav compose,
    -Navigation compose
    
    I also add simple home screen, to show the list of countries by name
    I also modified the response data
    I cleaned main activity
    
    Develop by Miguel Orozco Delgado <miguel.orozco.delgado@gmail.com>

--- app/src/main/java/com/morozco/mycaw/network/Model.kt
+++ app/src/main/java/com/morozco/mycaw/network/Model.kt
@@ -1,5 +1,22 @@
 package com.morozco.mycaw.network
 
 data class ItemResponse(
-    val capital: String? = null
+    val capital: String? = null,
+    val code: String? = null,
+    val currency: Currency? = null,
+    val flag: String? = null,
+    val language: Language? = null,
+    val name: String? = null,
+    val region: String? = null
+)
+
+data class Currency(
+    val code: String? = null,
+    val name: String? = null,
+    val symbol: String? = null
+)
+
+data class Language(
+    val code: String? = null,
+    val name: String? = null
 )
---
+++ app/src/main/java/com/morozco/mycaw/ui/CountriesScreens.kt
@@ -0,0 +1,81 @@
+package com.morozco.mycaw.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.morozco.mycaw.R
+import com.morozco.mycaw.ui.home.homeViewerScreen
+import com.morozco.mycaw.viewModel.CountryViewModel
+import java.lang.String.valueOf
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    currentScreen: String,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(title = { Text(currentScreen) }, colors = TopAppBarDefaults.mediumTopAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primaryContainer
+    ), modifier = modifier, navigationIcon = {
+        if (canNavigateBack) {
+            IconButton(onClick = navigateUp) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back_button)
+                )
+            }
+        }
+    })
+}
+
+@Composable
+fun CountriesApp(
+    viewModel: CountryViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    // Get current back stack entry
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    // Get the name of the current screen
+    val currentScreen = valueOf(
+        backStackEntry?.destination?.route ?: CountriesHome
+    )
+
+    Scaffold(topBar = {
+        TopBar(currentScreen = currentScreen,
+            canNavigateBack = navController.previousBackStackEntry != null,
+            navigateUp = { navController.navigateUp() })
+    }) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = CountriesHome,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
+        ) {
+            homeViewerScreen(viewModel)
+        }
+    }
+}
